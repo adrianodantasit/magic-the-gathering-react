2641c811576fc45f2cd04e67153d5d4d37a49546
Adriano <adrianodantasit@gmail.com>
2018-12-20 11:04:52 -0200

    First real test, checking if the color is valid

---
+++ src/__tests__/Search.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Search from '../js/Components/Search'
+
+test(`checking the option's colors`, () => {
+    const container = document.createElement('div');
+    ReactDOM.render(<Search />, container);
+    const options = container.querySelectorAll('option');
+    const optionsArr = Array.from(options).map(option => option.value);
+    //console.log(optionsArr);
+    expect(optionsArr).toContain("");
+    expect(optionsArr).toContain("White");
+    expect(optionsArr).toContain("Red");
+    expect(optionsArr).toContain("Black");
+    expect(optionsArr).toContain("Green");
+})

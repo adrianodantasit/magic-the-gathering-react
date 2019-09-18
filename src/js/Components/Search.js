14ac73c9c2ac4e1b38e17051d028d1cebde369ae
Adriano <adrianodantasit@gmail.com>
2019-09-18 10:27:02 -0300

    update test

--- src/js/Components/Search.js
+++ src/js/Components/Search.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+	render() {
+		const colors = [ 'White', 'Blue', 'Black', 'Red', 'Green', 'Purple' ];
 
-    render() {
-
-        const colors = ["White", "Blue", "Black", "Red", "Green"]
-
-        return (
-            <div className="select-colors">
-                <label htmlFor="color">
-                    Pick a color:
-                    <select id="color" value={this.props.color} onChange={this.props.handleColorChange} onBlur={this.props.handleColorChange}>
-                        <option />
-                        {colors.map(color =>
-                            <option key={color} value={color}> {color}</option>
-                        )}
-                    </select>
-                </label>
-            </div>);
-    }
+		return (
+			<div className="select-colors">
+				<label htmlFor="color">
+					Pick a color:
+					<select
+						id="color"
+						value={this.props.color}
+						onChange={this.props.handleColorChange}
+						onBlur={this.props.handleColorChange}
+					>
+						<option />
+						{colors.map((color) => (
+							<option key={color} value={color}>
+								{' '}
+								{color}
+							</option>
+						))}
+					</select>
+				</label>
+			</div>
+		);
+	}
 }
 
 export default Search;
